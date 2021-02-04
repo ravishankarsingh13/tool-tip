9902d6ae3637933d4deb46eac59e57f3117c8837
RAVI <ravi13eng@gmail.com>
2021-02-04 15:57:27 +0530

    completed

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+import '../css/App.css';
+
+import Button from './Button';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // state has position
+    this.state = { position: "top" };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // call on change of dropdown value
+  handleChange(event) {
+    this.setState({ position: event.target.value });
+  }
+
+  // call on Button click
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <form className="App" onSubmit={this.handleSubmit}>
+        <label>
+          <h3>Tooltip Position:</h3>
+          <select
+            className="dropdown"
+            value={this.state.position}
+            onChange={this.handleChange}
+          >
+            <option value="top">Top</option>
+            <option value="bottom">Bottom</option>
+            <option value="left">Left</option>
+            <option value="right">Right</option>
+          </select>
+        </label>
+
+        {/* component with passing props */}
+        <Button position={this.state.position} />
+      </form>
+    );
+  }
+}
+export default App;
